fa8a9bbf06387561fe1f08a3fe8a454299c6047c
SergioZamoras <sergioruizygarcia@gmail.com>
2018-04-19 13:13:09 +0200

    Codigo de clase PUNTO

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -6,16 +6,12 @@ package com.teaching.android;
 public class JavaApp {
 
     public static void main(String[] args) {
-        //New Persona("Messi,100f) nombre y dinero tienen que salir automatico, NO ESCRIBIRLO)
-        Persona p1 = new Persona ("Messi", 100f);
-        Persona p2 = new Persona("Ronaldo", 500f);
-        System.out.println("Ronaldo tiene " + p2.getDinero());
-        System.out.println("Messi tiene " + p1.getDinero());
-        p1.pagar (p2, 35f);
-        System.out.println("Ronaldo se queda con " + p2.getDinero());
-        System.out.println("Messi se queda con " + p1.getDinero());
-        Persona p3 = new Persona("Sergio", 643f);
-        System.out.println(p3.getNombre() + " jamas ha tenido " + p3.getDinero()+"€");
+        Punto p1 = new Punto(2,4);
+        Punto p2 = new Punto(3,5);
+    double distancia = p1.distancia(3,5);
+    System.out.println(distancia);
+    double distancia2 = p1.distancia(p2);
+    System.out.println(distancia2);
+    System.out.println(p1);
     }
-
 }
--- src/com/teaching/android/Punto.java
+++ src/com/teaching/android/Punto.java
@@ -1,14 +1,59 @@
 package com.teaching.android;
 
 public class Punto {
-    int x;
-    int y;
+    private int x;
+    private int y;
+
+    public Punto() {
+
+    }
+public Punto(int pX, int pY){
+        pX = x;
+        pY = y;
+}
 
     public int getX() {
         return x;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
     public int getY() {
         return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distancia(int pX, int pY){
+        int diferenciaX = pX - x;
+        int diferenciaY = pY - y;
+        double diferenciaXElevado = Math.pow(diferenciaX,2);
+        double diferenciaYElevado = Math.pow(diferenciaY,2);
+        double suma = diferenciaXElevado + diferenciaYElevado;
+        double resultado = Math.sqrt(suma);
+
+        return resultado;
+    }
+    public double distancia(Punto p){
+        int diferenciaX = p.x - x;
+        int diferenciaY = p.y - y;
+        double diferenciaXElevado = Math.pow(diferenciaX,2);
+        double diferenciaYElevado = Math.pow(diferenciaY,2);
+        double suma = diferenciaXElevado + diferenciaYElevado;
+        double resultado = Math.sqrt(suma);
+
+        return resultado;
+    }
+
+    @Override
+    public String toString() {
+        return "Punto{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
